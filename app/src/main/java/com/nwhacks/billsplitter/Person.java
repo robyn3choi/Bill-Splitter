12ac4718dd982945405f75851fc805de58d7a1b4
Tony Lee <zapman89@hotmail.com>
2016-02-28 02:57:14 -0800

    added SetUp Activity functionality, error checking

--- app/src/main/java/com/nwhacks/billsplitter/Person.java
+++ app/src/main/java/com/nwhacks/billsplitter/Person.java
@@ -7,28 +7,18 @@ import java.util.ArrayList;
  */
 public class Person {
 
-<<<<<<< HEAD
-    private String name;
-    private Double totalPrice;
-=======
     String name;
     Double totalPrice;
     ArrayList<Item> listOfItem;
->>>>>>> 5435269798f693f49bca9efce54c8d8f07e8f3be
 
     public Person(String name) {
         this.name = name;
         totalPrice = 0.00;
     }
 
-<<<<<<< HEAD
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
-=======
+    }
     public void addItem(Item item) {
         listOfItem.add(item);
     }
@@ -47,6 +37,5 @@ public class Person {
 
     public ArrayList<Item> getListOfItem() {
         return this.listOfItem;
->>>>>>> 5435269798f693f49bca9efce54c8d8f07e8f3be
     }
 }
--- app/src/main/java/com/nwhacks/billsplitter/SetUp.java
+++ app/src/main/java/com/nwhacks/billsplitter/SetUp.java
@@ -1,5 +1,7 @@
 package com.nwhacks.billsplitter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,13 +9,8 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
-=======
-import com.nwhacks.billsplitter.logic.Person;
 
-public class SetUp extends AppCompatActivity {
->>>>>>> 5435269798f693f49bca9efce54c8d8f07e8f3be
 
 public class SetUp extends AppCompatActivity {
 
@@ -21,29 +18,64 @@ public class SetUp extends AppCompatActivity {
     private ArrayList<Person> personList = new ArrayList<Person>();
     private EditText text;
     private PersonAdapter adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_up_listview);
         text = (EditText) findViewById(R.id.nameText);
         adapter = new PersonAdapter(this, R.layout.set_up_textview, personList);
-        ListView listView = (ListView) findViewById(R.id.list_person);
+        final ListView listView = (ListView) findViewById(R.id.list_person);
         listView.setAdapter(adapter);
         listView.setClickable(true);
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                return false;
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                {
+                    AlertDialog removeMessage = new AlertDialog.Builder(SetUp.this).create();
+                    removeMessage.setTitle("Delete");
+                    removeMessage.setMessage("Are you sure you want to delete this person?");
+                    removeMessage.setButton(AlertDialog.BUTTON_NEGATIVE, "NO",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int choice) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    removeMessage.setButton(AlertDialog.BUTTON_POSITIVE, "YES",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int choice) {
+                                    personList.remove(position);
+                                    adapter.notifyDataSetChanged();
+                                    dialog.dismiss();
+                                }
+                            });
+                    removeMessage.show();
+                    return false;
+                }
             }
         });
     }
     //adds a person to a list
     public void addPerson(View v){
         if(text.getText().toString().trim().length() != 0) {
-            String personName = text.getText().toString();
-            personList.add(new Person(personName));
-            adapter.notifyDataSetChanged();
-            text.setText("");
+            if(personList.size() < 12) {
+                String personName = text.getText().toString();
+                personList.add(new Person(personName));
+                adapter.notifyDataSetChanged();
+                text.setText("");
+            }
+            else{
+                AlertDialog tooManyMessage = new AlertDialog.Builder(SetUp.this).create();
+                tooManyMessage.setTitle("Too many people!");
+                tooManyMessage.setMessage("You can't have more than 12 people!");
+                tooManyMessage.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int choice) {
+                                dialog.dismiss();
+                            }
+                        });
+                tooManyMessage.show();
+            }
         }
     }
 }
