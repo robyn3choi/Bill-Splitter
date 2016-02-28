22b5fad8af6ef7516cf35e51182fe1a8494d9d96
Robyn <robyn3choi@gmail.com>
2016-02-28 01:35:39 -0800

    implemented drag and drop

--- app/src/main/java/com/nwhacks/billsplitter/MainActivity.java
+++ app/src/main/java/com/nwhacks/billsplitter/MainActivity.java
@@ -1,13 +1,23 @@
 package com.nwhacks.billsplitter;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -19,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Item> mealItems;
     ArrayList<Person> guests;
+    ImageView plateIcon;
+
+    String msg;
+    private android.widget.RelativeLayout.LayoutParams layoutParams;
 
 
     @Override
@@ -27,50 +41,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //for testing
-//        Person robyn = new Person("robyn");
-//        Person johnny = new Person("johnny");
-//        Person tony = new Person("tony");
-//        Person ian = new Person("ian");
-//        Person stuart = new Person("stuart");
-//        Person cam = new Person("cam");
-//        Person chris = new Person("chris");
-//        Person isaac = new Person("isaac");
-//        Person sam = new Person("sam");
-//        Person jlee = new Person("jlee");
-//        Person sarah = new Person("sarah");
-//        Person brian = new Person("brian");
-//
-//        guests = new ArrayList<Person>();
-//        guests.add(robyn);
-//        guests.add(johnny);
-//        guests.add(tony);
-//        guests.add(ian);
-//        guests.add(stuart);
-//        guests.add(cam);
-//        guests.add(chris);
-//        guests.add(isaac);
-//        guests.add(sam);
-//        guests.add(jlee);
-//        guests.add(sarah);
-//        guests.add(brian);
+        Person robyn = new Person("robyn");
+        Person johnny = new Person("johnny");
+        Person tony = new Person("tony");
+        Person ian = new Person("ian");
+        Person stuart = new Person("stuart");
+        Person cam = new Person("cam");
+        Person chris = new Person("chris");
+        Person isaac = new Person("isaac");
+        Person sam = new Person("sam");
+        Person jlee = new Person("jlee");
+        Person sarah = new Person("sarah");
+        Person brian = new Person("brian");
+
+        guests = new ArrayList<Person>();
+        guests.add(robyn);
+        guests.add(johnny);
+        guests.add(tony);
+        guests.add(ian);
+        guests.add(stuart);
+        guests.add(cam);
+        guests.add(chris);
+        guests.add(isaac);
+        guests.add(sam);
+        guests.add(jlee);
+        guests.add(sarah);
+        guests.add(brian);
 
 
+        setUpIcons();
+    }
 
+    private final class ImageTouchListener implements View.OnTouchListener {
 
-        CircleLayout circleLayout = (CircleLayout) findViewById(R.id.circleLayout);
-        for(int x=0; x < guests.size(); x++) {
-            ImageView image = new ImageView(MainActivity.this);
-            image.setBackgroundResource(R.mipmap.circle_icon);
-            image.setId(x);
-            circleLayout.addView(image);
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                //setup drag
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                //start dragging the item touched
+                view.startDrag(data, shadowBuilder, view, 0);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 
-            TextView guestName = new TextView(MainActivity.this);
-            guestName.setText(guests.get(x).getName());
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            params.addRule(RelativeLayout.ALIGN_LEFT,image.getId());
-            circleLayout.addView(guestName);
+    private class ImageDragListener implements View.OnDragListener {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            //handle the dragged view being dropped over a drop view
+            View view = (View) event.getLocalState();
+            //view dragged item is being dropped on
+            Button dropTarget = (Button) v;
+            //view being dragged and dropped
+            ImageView dropped = (ImageView) view;
+
+            //handle drag events
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    GradientDrawable drawable = (GradientDrawable)dropTarget.getBackground();
+                    drawable.setStroke(3, Color.parseColor("#00E1FF")); // set stroke width and stroke color
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+            return true;
         }
     }
 
@@ -99,6 +150,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void addMealItem(Item item) {
         mealItems.add(item);
+    }
+
+    public void setUpIcons() {
+
+        plateIcon = (ImageView) findViewById(R.id.plate_icon);
+        plateIcon.setOnTouchListener(new ImageTouchListener());
+
+        CircleLayout circleLayout = (CircleLayout) findViewById(R.id.circleLayout);
+        for(int x=0; x < guests.size(); x++) {
+
+            Button guestName = new Button(MainActivity.this);
+            guestName.setText(guests.get(x).getName());
+            guestName.setBackgroundResource(R.drawable.rounded_button);
+
+            final float scale = getResources().getDisplayMetrics().density;
+            int pixels = (int) (80 * scale + 0.5f);
+
+            CircleLayout.LayoutParams params = new CircleLayout.LayoutParams(pixels,pixels);
+            guestName.setLayoutParams(params);
+            circleLayout.addView(guestName);
+
+            guestName.setOnDragListener(new ImageDragListener());
+        }
     }
 
 
